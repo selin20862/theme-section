1626ba6e1f362c51cfd15138a8d9f887542a864b
DohaDerfouf <145472481+DohaDerfouf@users.noreply.github.com>
2023-10-17 16:43:11 +0200

    js

---
+++ js/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+    $(".dropdown-link").click(function(event) {
+        event.preventDefault(); // Empêche le lien de naviguer vers une autre page
+        var dropdownContent = $(this).next(".dropdown-conte"); 
+       
+        dropdownContent.slideToggle();
+        dropdownContent.toggleClass("custom-dropdown");
+     
+        $(".custom-dropdown").css({
+            "display": "flex",
+            "justify-content": "flex-end",
+            "flex-direction": "column"
+        });
+    });
+});
