371fe2f24cd06e0dda93dced6739635edaea1913
selin20862 <145472423+selin20862@users.noreply.github.com>
2023-10-19 13:19:02 +0200

    filtre

--- js/script.js
+++ js/script.js
@@ -17,3 +17,31 @@ $(document).ready(function() {
         });
     });
 });
+
+$(document).ready(function() {
+    const openBtn = $('.svg-filtre-open');
+    const closeBtn = $('.svg-filtre-close');
+    const filtre = $('.filtre');
+    const animationDuration = 0.2;
+
+    openBtn.on('click', function(e) {
+        e.preventDefault();
+        filtre.toggle();
+        if (filtre.is(':visible')) {
+            gsap.to(filtre, { duration: animationDuration, opacity: 1, ease: 'power2.out' });
+            closeBtn.toggle();
+            openBtn.toggle();
+        }
+    });
+
+    closeBtn.on('click', function(e) {
+        e.preventDefault();
+        gsap.to(filtre, { duration: animationDuration, opacity: 0, ease: 'power2.in', onComplete: hideFiltre });
+        closeBtn.toggle();
+        openBtn.toggle();
+    });
+
+    function hideFiltre() {
+        filtre.hide();
+    }
+});
