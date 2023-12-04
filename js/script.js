fcc46c0c0a26000949522a1ea7e84cee8f11c6fe
selin20862 <145472423+selin20862@users.noreply.github.com>
2023-12-04 14:21:22 +0100

    commit

--- js/script.js
+++ js/script.js
@@ -2,14 +2,14 @@
 
 
 // info
-$(document).ready(function() {
-    $(".dropdown-link").click(function(event) {
+$(document).ready(function () {
+    $(".dropdown-link").click(function (event) {
         event.preventDefault(); // Empêche le lien de naviguer vers une autre page
-        var dropdownContent = $(this).next(".dropdown-conte"); 
-       
+        var dropdownContent = $(this).next(".dropdown-conte");
+
         dropdownContent.slideToggle();
         dropdownContent.toggleClass("custom-dropdown");
-     
+
         $(".custom-dropdown").css({
             "display": "flex",
             "justify-content": "flex-end",
@@ -18,13 +18,13 @@ $(document).ready(function() {
     });
 });
 
-$(document).ready(function() {
+$(document).ready(function () {
     const openBtn = $('.svg-filtre-open');
     const closeBtn = $('.svg-filtre-close');
     const filtre = $('.filtre');
     const animationDuration = 0.2;
 
-    openBtn.on('click', function(e) {
+    openBtn.on('click', function (e) {
         e.preventDefault();
         filtre.toggle();
         if (filtre.is(':visible')) {
@@ -34,7 +34,7 @@ $(document).ready(function() {
         }
     });
 
-    closeBtn.on('click', function(e) {
+    closeBtn.on('click', function (e) {
         e.preventDefault();
         gsap.to(filtre, { duration: animationDuration, opacity: 0, ease: 'power2.in', onComplete: hideFiltre });
         closeBtn.toggle();
@@ -48,7 +48,7 @@ $(document).ready(function() {
     var imageWidth = $('.section-detail img').width();
     var containerWidth = $('.section-detail').width();
 
-    if (window.innerWidth > 992 && imageWidth > 0.5 * containerWidth) {
+    if (window.innerWidth >= 992 && imageWidth > 0.5 * containerWidth) {
         console.log('L\'image est plus grande que 50% du conteneur');
 
         $('.div-detail').css({
@@ -57,16 +57,16 @@ $(document).ready(function() {
             'bottom': '0',
             'width': '50%'
         });
-    
-        $('.div-detail a:first-child').on('click', function(e) {
+
+        $('.div-detail a:first-child').on('click', function (e) {
             e.preventDefault();
             $(this).fadeOut();
             $('.div-detail a:nth-child(2)').fadeIn().css('display', 'flex');
             $('.div-detail h1, .div-detail p').fadeOut();
             $('.div-detail').animate({ width: '30%', height: '30%' });
         });
-    
-        $('.div-detail a:nth-child(2)').on('click', function(e) {
+
+        $('.div-detail a:nth-child(2)').on('click', function (e) {
             e.preventDefault();
             $(this).fadeOut();
             $('.div-detail a:first-child').fadeIn();
@@ -82,6 +82,47 @@ $(document).ready(function() {
         });
     }
 
-    
+
+    if (window.innerWidth >= 992) {
+        const sectionTravaux = document.querySelector('.sectiontravaux');
+        let sectionTravauxWidth = sectionTravaux.offsetWidth;
+        let amountToScroll = sectionTravauxWidth - window.innerWidth;
+
+        gsap.to(sectionTravaux, {
+            x: -amountToScroll,
+            scrollTrigger: {
+                trigger: sectionTravaux,
+                start: "top top",
+                end: "+=100%",
+                scrub: 1,
+                pin: true,
+                anticipatePin: 1
+            }
+        });
+
+        const sectionProjets = document.querySelector('.sectionprojets');
+        let sectionProjetsWidth = sectionProjets.offsetWidth;
+        let amountToScroll2 = sectionProjetsWidth - window.innerWidth;
+
+        gsap.to(sectionProjets, {
+            x: -amountToScroll2,
+            scrollTrigger: {
+                trigger: sectionProjets,
+                start: "top top",
+                end: "+=100%",
+                scrub: 1,
+                pin: true,
+                anticipatePin: 1
+            }
+        });
+
+    }
+
+
+
+
+
+
+
 });
 
